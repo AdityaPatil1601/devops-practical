827a30cd9c30f62f43d7d3a3d9a124e04a777fa3
AdityaPatil1601 <adityapatil111000>
2026-04-02 11:44:16 +0530

    Updated Data

--- app.py
+++ app.py
@@ -6,9 +6,10 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return "Hello! DevOps Practical Running Successfully"
+    return "My name is Aditya"
+    return "I live in Malad(w)"
+    return "Study in Dept CMPN"
 
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
-
-
